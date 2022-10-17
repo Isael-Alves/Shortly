0388a2423765b28ec23c745a42edd5d5fe9f1e57
isael lopes <isael.alves@turucui.ufpa.br>
2022-10-17 14:02:33 -0300

    ranking pronto

---
+++ controllers/rankingController.js
@@ -0,0 +1,21 @@
+import db from "../database/db.js";
+
+async function getRanking(req, res) {
+  try {
+    const { rows } = await db.query(`
+    SELECT users.id, users.name, COUNT(urls.id) as "linksCount", SUM(urls."visitCount") as "visitCount"
+    FROM urls
+    JOIN users ON urls."userId" = users.id
+    GROUP BY users.id
+    ORDER BY "visitCount" DESC
+    LIMIT 10
+  `);
+  const ranking = rows;
+  return res.send(ranking);
+  } catch (e) {
+    console.log(e);
+    res.sendStatus(500);
+  }
+}
+
+export default getRanking;
